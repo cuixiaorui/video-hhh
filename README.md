94df80be14247c0da76ae3ab125911ab214c01cc
cuixiaorui <cui_xiaorui@126.com>
2020-10-19 18:57:46 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+<h1 align="center">Welcome to video-hhh 👋</h1>
+<p>
+  <a href="https://www.npmjs.com/package/video-hhh" target="_blank">
+    <img alt="Version" src="https://img.shields.io/npm/v/video-hhh.svg">
+  </a>
+  <a href="#" target="_blank">
+    <img alt="License: ISC" src="https://img.shields.io/badge/License-ISC-yellow.svg" />
+  </a>
+</p>
+
+> video-hhh
+
+可用于统计视频时长
+
+### 🏠 [Homepage](https://github.com/cuixiaorui/koa-setup)
+
+## Install
+
+```sh
+npm install video-hhh -g
+```
+
+## Usage
+
+```sh
+video-hhh "video-path"
+
+// 举个栗子
+video-hhh "./example/*.mp4"
+```
+
+> video-path 支持 [glob](https://www.npmjs.com/package/glob) 格式的路径
+
+## Author
+
+👤 **cuixiaorui**
+
+- Website: beijing
+- Github: [@cuixiaorui](https://github.com/cuixiaorui)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+---
+
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ bin/index.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+const chalk = require("chalk");
+const glob = require("glob");
+const { getVideoDurationInSeconds } = require("get-video-duration");
+
+function toAbsolutePath(files) {
+  const path = require("path");
+  return files.map((file) => {
+    return path.resolve(__dirname, file);
+  });
+}
+
+async function calculateVideoTime(files) {
+  let totalTime = 0;
+  for (const file of files) {
+    const duration = await getVideoDurationInSeconds(file);
+    totalTime += duration;
+
+    console.log(
+      chalk
+        .hex("#DEADED")
+        .bold(`${file}:\r\n`, formatDate(secondToHour(duration)))
+    );
+  }
+
+  return totalTime;
+}
+
+function secondToHour(second) {
+  let s = 0;
+  let m = parseInt(second / 60);
+  s = Math.ceil(second % 60);
+
+  let h = parseInt(m / 60);
+  m = Math.ceil(m % 60);
+
+  return {
+    s,
+    m,
+    h,
+  };
+}
+
+function formatDate(date) {
+  const { s, m, h } = date;
+  const sStr = s < 10 ? "0" + s : s;
+  const mStr = m < 10 ? "0" + m : m;
+  const hStr = h < 10 ? "0" + h : h;
+
+  return `${hStr}:${mStr}:${sStr}`;
+}
+
+(async function main() {
+  const pattern = process.argv.slice(2)[0];
+  const files = toAbsolutePath(glob.sync(pattern));
+  const totalTime = await calculateVideoTime(files);
+
+  console.log(
+    chalk
+      .hex("#ffff00")
+      .bold("总时长：\r\n", formatDate(secondToHour(totalTime)))
+  );
+})();
